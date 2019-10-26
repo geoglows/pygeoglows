81e65d8559ffa809b39e065fa40a95a40e227916
rileyhales <rileyhales1@gmail.com>
2019-10-25 18:57:09 -0600

    loosen restrictions on python and package versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="geoglows",
-    version="1",
+    version="0.0.1",
     author="Riley Hales",
     license='BSD 3-Clause',
     description="Package for accessing data and APIs developed for the GEOGloWS initiative",
