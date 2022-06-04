63940a15dbecc5060a1a3fa8118225c0883afdb1
Riley Hales <rileyhales1@gmail.com>
2022-06-04 10:57:31 -0600

    increment version number

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ copyright = '2021, Riley Hales'
 author = 'Riley Hales'
 
 # The full version, including alpha/beta/rc tags
-release = '0.26.1'
+release = '0.27.0'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
