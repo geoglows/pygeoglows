80d115c78decf195277a150760a0b9d8f70ad370
Riley Hales <rileyhales1@gmail.com>
2023-11-20 17:21:04 -0700

    allow printing urls, parse datetimes

--- geoglows/data.py
+++ geoglows/data.py
@@ -1,16 +1,16 @@
-import os
-
 import pandas as pd
 import requests
 import s3fs
 import xarray as xr
 
+from io import StringIO
+
 __all__ = [
+    'dates',
     'forecast',
     'forecast_stats',
     'forecast_ensembles',
     'forecast_records',
-    'forecast_dates',
     'retrospective',
     'daily_averages',
     'monthly_averages',
@@ -25,15 +25,19 @@ ODP_S3_BUCKET_REGION = 'us-west-2'
 
 
 def _forecast_endpoint_decorator(function):
-    def wrapper(**kwargs):
+    def wrapper(*args, **kwargs):
         # update the default values set by the function unless the user has already specified them
-        for key, value in function.__kwdefaults__.items():
+        for key, value in function.__kwdefaults__.items() if function.__kwdefaults__ else []:
             if key not in kwargs:
                 kwargs[key] = value
+
+        return_url = kwargs.get('format', 'csv') == 'url'
 
         # parse out the information necessary to build a request url
         endpoint = kwargs.get('endpoint', DEFAULT_REST_ENDPOINT)
         endpoint = endpoint[:-1] if endpoint[-1] == '/' else endpoint
+        endpoint = endpoint + '/api' if not endpoint.endswith('/api') else endpoint
+        endpoint = f'https://{endpoint}' if not endpoint.startswith(('https://', 'http://')) else endpoint
 
         version = kwargs.get('version', DEFAULT_REST_ENPOINT_VERSION)
 
@@ -46,7 +50,7 @@ def _forecast_endpoint_decorator(function):
         assert return_format in ('csv', 'json', 'url'), f'Unsupported return format requested: {return_format}'
 
         # request parameter validation before submitting
-        for key in ('endpoint', 'version'):
+        for key in ('endpoint', 'version', 'reach_id'):
             if key in kwargs:
                 del kwargs[key]
         for key, value in kwargs.items():
@@ -55,26 +59,30 @@ def _forecast_endpoint_decorator(function):
         for date in ('date', 'start_date', 'end_date'):
             if date in kwargs:
                 assert pd.to_datetime(kwargs[date], format='%Y%m%d'), f'{date} must be a string in YYYYMMDD format'
-        if 'return_format' in kwargs and kwargs['return_format'] != 'json':
-            del kwargs['return_format']  # csv is the default, url is only a python function response
+        if 'format' in kwargs and kwargs['format'] != 'json':
+            del kwargs['format']
         kwargs['source'] = kwargs.get('source', 'pygeoglows')  # allow using default for specific apps which override
         params = '&'.join([f'{key}={value}' for key, value in kwargs.items()])
 
         # piece together the request url
         request_url = f'{endpoint}/{version}/{product_name}{reach_id}?{params}'
 
-        if return_format == 'url':
-            return request_url
+        if return_url:
+            return request_url.replace(f'source={kwargs["source"]}', '')
 
         response = requests.get(request_url)
 
         if response.status_code != 200:
-            raise RuntimeError('Recieved an error from the Streamflow REST API: ' + response.text)
+            raise RuntimeError('Received an error from the Streamflow REST API: ' + response.text)
 
-        if return_format == 'json':
+        if return_format == 'csv':
+            df = pd.read_csv(StringIO(response.text))
+            if 'datetime' in df.columns:
+                df['datetime'] = pd.to_datetime(df['datetime'])
+                df = df.set_index('datetime')
+            return df
+        elif return_format == 'json':
             return response.json()
-        elif return_format == 'csv':
-            return pd.read_csv(response.text)
         else:
             raise ValueError(f'Unsupported return format requested: {return_format}')
 
@@ -83,12 +91,12 @@ def _forecast_endpoint_decorator(function):
 
 # Forecast data and derived products
 @_forecast_endpoint_decorator
-def forecast_dates(**kwargs) -> dict or str:
+def dates(**kwargs) -> dict or str:
     """
     Gets a list of available forecast product dates
 
     Keyword Args:
-        return_format: csv, json, or url, default csv
+        format: csv, json, or url, default csv
         endpoint: the endpoint of an api instance, only applicable for package or rest endpoint developers
 
     Returns:
@@ -108,7 +116,7 @@ def forecast(*, reach_id: int, **kwargs) -> pd.DataFrame or dict or str:
     Keyword Args:
         reach_id: the ID of a stream, should be a 9 digit integer
         date: a string specifying the date to request in YYYYMMDD format, returns the latest available if not specified
-        return_format: csv, json, or url, default csv
+        format: csv, json, or url, default csv
         endpoint: the endpoint of an api instance, only applicable for package or rest endpoint developers
 
     Returns:
@@ -126,7 +134,7 @@ def forecast_stats(*, reach_id: int, **kwargs) -> pd.DataFrame or dict or str:
     Keyword Args:
         reach_id: the ID of a stream, should be a 9 digit integer
         date: a string specifying the date to request in YYYYMMDD format, returns the latest available if not specified
-        return_format: csv, json, or url, default csv
+        format: csv, json, or url, default csv
         endpoint: the endpoint of an api instance, only applicable for package or rest endpoint developers
 
     Returns:
@@ -143,7 +151,7 @@ def forecast_ensembles(*, reach_id: int, **kwargs) -> pd.DataFrame or dict or st
     Keyword Args:
         reach_id: the ID of a stream, should be a 9 digit integer
         date: a string specifying the date to request in YYYYMMDD format, returns the latest available if not specified
-        return_format: csv, json, or url, default csv
+        format: csv, json, or url, default csv
         endpoint: the endpoint of an api instance, only applicable for package or rest endpoint developers
 
     Returns:
@@ -161,7 +169,7 @@ def forecast_records(*, reach_id: int, **kwargs) -> pd.DataFrame or dict or str:
         reach_id: the ID of a stream, should be a 9 digit integer
         start_date: a YYYYMMDD string giving the earliest date this year to include, defaults to YYYY0101
         end_date: a YYYYMMDD string giving the latest date this year to include, defaults to latest available
-        return_format: csv, json, or url, default csv
+        format: csv, json, or url, default csv
         endpoint: the endpoint of an api instance, only applicable for package or rest endpoint developers
 
     Returns:
@@ -185,7 +193,8 @@ def retrospective(reach_id: int) -> pd.DataFrame:
     """
     s3 = s3fs.S3FileSystem(anon=True, client_kwargs=dict(region_name=ODP_S3_BUCKET_REGION))
     s3store = s3fs.S3Map(root=f'{ODP_S3_BUCKET_URI}/retro.zarr', s3=s3, check=False)
-    return xr.open_zarr(s3store).sel(rivid=reach_id).to_dataframe().reset_index().set_index('time').pivot(columns='rivid', values='Qout')
+    return xr.open_zarr(s3store).sel(rivid=reach_id).to_dataframe().reset_index().set_index('time').pivot(
+        columns='rivid', values='Qout')
 
 
 def historical(*args, **kwargs):
