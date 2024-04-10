7873f2380c232d0f517c04b90c03910e462625e2
Riley Hales PhD <39097632+rileyhales@users.noreply.github.com>
2024-04-10 16:35:23 -0600

    get many rivids at once from aws

--- geoglows/__init__.py
+++ geoglows/__init__.py
@@ -11,6 +11,6 @@ __all__ = [
     'bias', 'plots', 'data', 'analyze', 'streams', 'tables',
     'METADATA_TABLE_PATH'
 ]
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __author__ = 'Riley Hales'
 __license__ = 'BSD 3-Clause Clear License'
