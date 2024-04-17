34e33ca36b21ef317b68c5072db1bbe666b56178
rileyhales <rileyhales1@gmail.com>
2024-04-17 13:14:18 -0600

    update version number

--- geoglows/__init__.py
+++ geoglows/__init__.py
@@ -12,6 +12,6 @@ __all__ = [
     'bias', 'plots', 'data', 'analyze', 'streams', 'tables', 'streamflow',
     'METADATA_TABLE_PATH'
 ]
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 __author__ = 'Riley Hales'
 __license__ = 'BSD 3-Clause Clear License'
