069e0737d577c96b8bcb7edd853cffd4cc5dcf37
rileyhales <rileyhales1@gmail.com>
2025-02-13 09:23:30 -0700

    increment version number

--- geoglows/__init__.py
+++ geoglows/__init__.py
@@ -1,17 +1,15 @@
-import geoglows.bias
 import geoglows._plots as plots
-import geoglows.data
 import geoglows.analyze
+import geoglows.bias
+import geoglows.data
+import geoglows.streamflow
 import geoglows.streams
 import geoglows.tables
-import geoglows.streamflow
-
 from ._constants import get_metadata_table_path, set_metadata_table_path
 
 __all__ = [
     'bias', 'plots', 'data', 'analyze', 'streams', 'tables', 'streamflow',
-    'get_metadata_table_path', 'set_metadata_table_path',
 ]
-__version__ = '1.8.1'
+__version__ = '2.0.0'
 __author__ = 'Riley Hales'
 __license__ = 'BSD 3-Clause Clear License'
