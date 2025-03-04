1adc54eea4c9074a9dc5ba49dcde4e0f610682ad
rileyhales <rileyhales1@gmail.com>
2025-03-04 10:53:49 -0700

    remove deprecated model version 1 module

--- geoglows/__init__.py
+++ geoglows/__init__.py
@@ -3,13 +3,12 @@ import geoglows._plots as plots
 import geoglows.data
 import geoglows.analyze
 import geoglows.tables
-import geoglows.streamflow
 
 from ._constants import set_uri
 from ._constants import get_uri
 
 __all__ = [
-    'bias', 'plots', 'data', 'analyze', 'tables', 'streamflow',
+    'bias', 'plots', 'data', 'analyze', 'tables',
     'set_uri', 'get_uri'
 ]
 __version__ = '2.0.0'
